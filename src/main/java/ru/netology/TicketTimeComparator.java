7af7f5a1de9494532e1036645e1db718addf2e9d
Gudov Dmitry <dgudoff@yandex.ru>
2023-06-22 21:54:54 +0300

    Initial commit

---
+++ src/main/java/ru/netology/TicketTimeComparator.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        int FlightTimeOne;
+        int FlightTimeTwo;
+
+
+        if ((t1.getTimeTo() - t1.getTimeFrom()) < 0) {
+            FlightTimeOne = 24 - (t1.getTimeTo() - t1.getTimeFrom());
+        } else {
+            FlightTimeOne = t1.getTimeTo() - t1.getTimeFrom();
+        }
+
+        if ((t2.getTimeTo() - t2.getTimeFrom()) < 0) {
+            FlightTimeTwo = 24 - (t2.getTimeTo() - t2.getTimeFrom());
+        } else {
+            FlightTimeTwo = t2.getTimeTo() - t2.getTimeFrom();
+        }
+
+        if (FlightTimeOne < FlightTimeTwo) {
+            return -1;
+        } else if (FlightTimeOne > FlightTimeTwo) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
